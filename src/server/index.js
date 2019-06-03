4164a8e98a2fe87092e531b9d1983b47bdd037df
Andrew McGill <arm5077@gmail.com>
2019-06-02 21:03:35 -0400

    Add /api/wormhole/

--- src/server/index.js
+++ src/server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const getStories = require('./getStoriesFromRSS');
 const filterStories = require('./filterStories.js');
+const shuffle = require('shuffle-array');
+const fs = require('fs');
 
 app.use(express.static('client'));
 app.use(express.json())
@@ -17,6 +19,14 @@ app.post('/api/stories', async function (req, res) {
   let stories = await getStories();
   stories = filterStories(stories, ids);
   res.json(stories);
+});
+
+app.post('/api/wormhole', function(req, res) {
+  const wormholeStories = JSON.parse(fs.readFileSync(`${__dirname}/data/stories.json`, 'utf8'));
+  const ids = req.body.ids || [];
+  let stories = wormholeStories.filter(story => ids.indexOf(parseInt(story.id)) == -1);
+  shuffle(stories);
+  res.json(stories.slice(0,25));
 });
 
 app.listen(process.env.PORT || 61386, () => console.log(`Listening on port ${process.env.PORT || 61386}!`));
